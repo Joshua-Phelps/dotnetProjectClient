374bf74c1d10bcf04d5c2f0ca998d36fd14bfcfd
jugtuttle <jugtuttle@gmail.com>
2020-10-16 18:17:12 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [values, setValues] = useState([]);
+
+	useEffect(() => {
+		axios.get('http://localhost:5000/api/values').then(res => {
+			console.log(res);
+			setValues(res.data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<Header as='h2'>
+				<Icon name='users' />
+				<Header.Content>Reactivities</Header.Content>
+			</Header>
+			<List>
+				<List.Item>Apples</List.Item>
+			</List>
+		</div>
+	);
 }
 
 export default App;
