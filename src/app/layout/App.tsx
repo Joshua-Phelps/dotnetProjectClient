fa50efb8c40b0cadfcd37528be67184161ed2539
jugtuttle <jugtuttle@gmail.com>
2020-10-20 15:14:19 -0700

    end of section 6

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, SyntheticEvent } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 const App = () => {
 	const [activities, setActivities] = useState<IActivity[]>([]);
@@ -12,6 +13,9 @@ const App = () => {
 		null
 	);
 	const [editMode, setEditMode] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [submitting, setSubmitting] = useState(false);
+	const [target, setTarget] = useState('');
 
 	const handleSelectActivity = (id: string) => {
 		setSelectedActivity(activities.filter(a => a.id === id)[0]);
@@ -24,35 +28,56 @@ const App = () => {
 	};
 
 	const handleCreateActivity = (activity: IActivity) => {
-		setActivities([...activities, activity]);
-		setSelectedActivity(activity);
-		setEditMode(false);
+		setSubmitting(true);
+		agent.Activities.create(activity)
+			.then(() => {
+				setActivities([...activities, activity]);
+				setSelectedActivity(activity);
+				setEditMode(false);
+			})
+			.then(() => setSubmitting(false));
 	};
 
 	const handleEditActivity = (activity: IActivity) => {
-		setActivities([
-			...activities.filter(a => a.id !== activity.id),
-			activity,
-		]);
-		setSelectedActivity(activity);
-		setEditMode(false);
+		setSubmitting(true);
+		agent.Activities.update(activity)
+			.then(() => {
+				setActivities([
+					...activities.filter(a => a.id !== activity.id),
+					activity,
+				]);
+				setSelectedActivity(activity);
+				setEditMode(false);
+			})
+			.then(() => setSubmitting(false));
 	};
 
-	const handleDeleteActivity = (id: string) => {
-		setActivities([...activities.filter(a => a.id !== id)]);
+	const handleDeleteActivity = (
+		e: SyntheticEvent<HTMLButtonElement>,
+		id: string
+	) => {
+		setSubmitting(true);
+		setTarget(e.currentTarget.name);
+		agent.Activities.delete(id)
+			.then(() => {
+				setActivities([...activities.filter(a => a.id !== id)]);
+			})
+			.then(() => setSubmitting(false));
 	};
 
 	useEffect(() => {
-		axios
-			.get<IActivity[]>('http://localhost:5000/api/activities')
+		agent.Activities.list()
 			.then(res => {
-				let activities = res.data.map(activity => {
+				let activities = res.map(activity => {
 					activity.date = activity.date.split('.')[0];
 					return activity;
 				});
 				setActivities(activities);
-			});
+			})
+			.then(() => setLoading(false));
 	}, []);
+
+	if (loading) return <LoadingComponent content='Loading activity...' />;
 
 	return (
 		<>
@@ -68,6 +93,8 @@ const App = () => {
 					createActivity={handleCreateActivity}
 					editActivity={handleEditActivity}
 					deleteActivity={handleDeleteActivity}
+					submitting={submitting}
+					target={target}
 				/>
 			</Container>
 		</>
--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ interface IProps {
 	activity: IActivity;
 	createActivity: (activity: IActivity) => void;
 	editActivity: (activity: IActivity) => void;
+	submitting: boolean;
 }
 
 export const ActivityForm: React.FC<IProps> = ({
@@ -15,6 +16,7 @@ export const ActivityForm: React.FC<IProps> = ({
 	activity: initialFormState,
 	createActivity,
 	editActivity,
+	submitting,
 }) => {
 	const initializeForm = () => {
 		if (initialFormState) return initialFormState;
@@ -87,6 +89,7 @@ export const ActivityForm: React.FC<IProps> = ({
 					name='venue'
 				/>
 				<Button
+					loading={submitting}
 					floated='right'
 					positive
 					type='submit'
